6a444287bbebc75b0b3f117d9d5079c73877a200
Vterebenin <niceserver11@mail.ru>
2018-08-01 21:40:12 +0300

    solve 6kyu

--- spinWords.js
+++ spinWords.js
@@ -5,3 +5,24 @@ of this Kata). Strings passed in will consist of only letters and spaces. Spaces
 will be included only when more than one word is present.
 */
 
+function spinWords(string) {
+	var answer = []
+  string.split(" ").map(reverseIfFive)
+  function reverseIfFive(value, index, array) {
+ 		if (value.length >= 5) {
+ 			value = value.split("").reverse().join("");
+ 		} 
+		answer.push(value)
+  }
+  return answer.join(" ")
+}
+
+console.log(spinWords("take a five words string"))
+
+//BP
+
+function spinWords(words){
+  return words.split(' ').map(function (word) {
+    return (word.length > 4) ? word.split('').reverse().join('') : word;
+  }).join(' ');
+}
